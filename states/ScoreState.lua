f70987ac06141b209cf987247f7361ac01e1fa04
reiitenshi <80589127+reiitenshi@users.noreply.github.com>
2023-03-16 11:01:46 +1100

    Add files via upload

---
+++ states/ScoreState.lua
@@ -0,0 +1,58 @@
+--[[
+    ScoreState Class
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    A simple state used to display the player's score before they
+    transition back into the play state. Transitioned to from the
+    PlayState when they collide with a Pipe.
+]]
+
+ScoreState = Class{__includes = BaseState}
+
+--[[
+    When we enter the score state, we expect to receive the score
+    from the play state so we know what to render to the State.
+]]
+function ScoreState:enter(params)
+    self.score = params.score
+
+    self.gold = love.graphics.newImage('gold.png')
+    self.silver = love.graphics.newImage('silver.png')
+    self.bronze = love.graphics.newImage('bronze.png')
+end
+
+function ScoreState:update(dt)
+    -- go back to play if enter is pressed
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+function ScoreState:render()
+    -- simply render the score to the middle of the screen
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+
+    local received = nil
+    local type = nil
+    if self.score >= 5 then
+        received = self.gold
+        type = 'gold'
+    elseif self.score >= 1 then
+        received = self.silver
+        type = 'silver'
+    elseif self.score == 0 then
+        received = self.bronze
+        type = 'bronze'
+    end
+
+    if received ~= nil then
+        love.graphics.draw(received, VIRTUAL_WIDTH / 2 - received:getWidth() / 2, 120)
+    end
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+end
